3f16b3331a2476c8d334a1996651ea789bc0fb3f
javaitstu <93223183+javaitstu@users.noreply.github.com>
2021-10-28 20:02:24 +0800

    Create Solution.java
    判断数组中只出现一次的数字

---
+++ day01/只出现一次的数字/Solution.java
@@ -0,0 +1,17 @@
+package com.只出现一次的数字;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {4,1,2,1,2};
+        System.out.println(solution.singleNumber(nums));
+    }
+    public int singleNumber(int[] nums) {
+        int reduce = 0;
+        for (int num : nums) {
+            reduce =  reduce ^ num;
+        }
+        return reduce;
+
+    }
+}
