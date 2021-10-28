b99c11d764795c774a6e77df19cacedaf2b780ed
javaitstu <93223183+javaitstu@users.noreply.github.com>
2021-10-28 20:03:17 +0800

    Create Solution.java
    将数组向右移动，最后一位移到第一位

---
+++ day01/旋转数组/Solution.java
@@ -0,0 +1,32 @@
+package com.旋转数组;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1,2};
+        solution.rotate(nums,3);
+        for (int i = 0; i < nums.length; i ++) {
+            System.out.println(nums[i]);
+        }
+    }
+
+    public void rotate(int[] nums, int k) {
+        int len = nums.length;
+        k = k % len;
+        if(k != 0){
+            int[] temp = new int[k];
+            int l = 0;
+            for(int i = len - k; i < len; i ++) {
+                temp[l++] = nums[i];
+            }
+            for(int j = len - k - 1; j >= 0; j--){
+                nums[j+k] = nums[j];
+            }
+            for(int i = 0; i < k; i ++) {
+                nums[i] = temp[i];
+            }
+        }
+
+    }
+}
